97ce385c50212748a7eb51b241ba4f5fd3cbae4a
ksoltan <kattsoltan@gmail.com>
2018-11-26 23:51:03 -0500

    Roadmap implemented with visualization. See TODOs. Most important one is to tweak standard deviations and number of states to provide meaningful transitions.

---
+++ mdp_planner/src/State.py
@@ -0,0 +1,82 @@
+import math
+import rospy
+from visualization_msgs.msg import Marker
+from geometry_msgs.msg import Vector3, Pose
+
+'''
+    Class: State
+
+    A state represents the possible position (x, y) and orientation (theta)
+    of the robot in the map.
+
+'''
+class State(object):
+    def __init__(self, x=None, y=None, theta=None):
+        self.x = x
+        self.y = y
+        self.theta = theta # Radians
+
+    '''
+        Function: distance
+        Inputs: State other
+
+        Returns the weighted distance between two states,
+        including both position and orientation.
+    '''
+    def distance(self, other):
+        alpha = 1 # TODO: Check how to weight relative orientation and distance
+        return math.sqrt((self.x - other.x)**2 +
+                         (self.y - other.y)**2 +
+                         alpha*(self.theta - other.theta)**2)
+
+    def get_marker(self, r=0.0, g=1.0, b=0.0, scale=0.3):
+        marker = Marker()
+        marker.header.stamp = rospy.Time.now()
+        marker.header.frame_id = "map"
+
+        marker.type = Marker.SPHERE
+        marker.color.a = 0.7
+        marker.color.r = r
+        marker.color.g = g
+        marker.color.b = b
+
+        marker.scale.x = scale
+        marker.scale.y = scale
+        marker.scale.z = scale
+
+        marker.pose.position.x = self.x
+        marker.pose.position.y = self.y
+        marker.lifetime = rospy.Time(2)
+
+        return marker
+
+    def get_pose(self):
+        pose = Pose()
+        pose.position.x = self.x
+        pose.position.y = self.y
+        # Euler to Quaternion angle
+        pose.orientation.w = math.cos(self.theta / 2.0)
+        pose.orientation.z = math.sin(self.theta / 2.0)
+
+        return pose
+
+    def get_distance_vector(self, other):
+        marker = Marker()
+        marker.header.stamp = rospy.Time.now()
+        marker.header.frame_id = "map"
+
+        marker.type = Marker.ARROW
+        marker.color.a = 0.7
+        marker.color.g = 1.0
+
+        marker.scale.x = 0.05
+        marker.scale.y = 0.1
+        marker.scale.z = 0.05
+
+        marker.points = [Vector3(self.x, self.y, 0), Vector3(other.x, other.y, 0)]
+        marker.lifetime = rospy.Time(2)
+
+        return marker
+
+    def __str__(self):
+        return "State: x, y, theta - ({}, {}, {} deg)".format(self.x, self.y, math.degrees(self.theta))
