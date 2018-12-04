d9091870d65a0012e17a2128badd4906c8d1503e
ksoltan <kattsoltan@gmail.com>
2018-12-04 15:30:53 -0500

    Probably just num_positions/orientation changes

--- mdp_planner/src/markov_model.py
+++ mdp_planner/src/markov_model.py
@@ -224,6 +224,26 @@ class MarkovModel(object):
         return self.roadmap[start_state_idx][end_state_idx][action]
 
     def is_collision_free_path(self, start_state_idx, end_state_idx):
+        for angle in np.linspace(0, 2 * math.pi, num_circle_points):
+            circle_point = (robot_radius * math.cos(angle) + point[0], robot_radius * math.sin(angle) + point[1])
+            x_coord = int((circle_point[0] - self.map.info.origin.position.x) / self.map.info.resolution)
+            y_coord = int((circle_point[1] - self.map.info.origin.position.y) / self.map.info.resolution)
+
+            # check if we are in bounds
+            # Used in robot_localization/robot_localizer/scripts/occupancy_field.py
+            if x_coord > self.map.info.width or x_coord < 0:
+                return False
+            if y_coord > self.map.info.height or y_coord < 0:
+                return False
+
+            ind = x_coord + y_coord * self.map.info.width
+
+            if ind >= self.map.info.width*self.map.info.height or ind < 0:
+                return False
+
+            is_occupied = self.map.data[ind]
+            if is_occupied:
+                return False
         # TODO: probably fine for small enough motion, but should implement a raytrace type thing.
         return True
 
