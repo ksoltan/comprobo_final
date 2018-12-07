b9e509a9a3f4d239fd9004af2e86ffe919c17374
Maxmilian626 <maxmilian.wei@students.olin.edu>
2018-12-07 15:15:30 -0500

    tester

--- slam/slam.py
+++ slam/slam.py
@@ -47,7 +47,6 @@ def map_from_scan(scan, pose, resolution, max_scan):
 	return Map(map_, resolution)
 
 class Map():
-<<<<<<< HEAD
 	def __init__(self, size): #Size is a tuple of [x,y] in meters.
 		self.resolution = 0.05
 		self.map_ = create_empty_map(size, self.resolution)
@@ -56,11 +55,11 @@ class Map():
 		self.origin = (math.floor(self.size[0]/2), math.floor(self.size[1]/2))
         self.pose = [] #[x, y, theta], is always relative.  It's the measure of distance from origin.
 
-	    def stitch(scan_map):
+	    def stitch(self, scan_map):
 	        #Take current map, reference to origin.  Compare to scan, reference to pose.
 	        #Transform (only location) scan to have the same orientation as origin, and merge.
-			bound_check(scan_map)
-			scan_size = len(scan)
+			self.bound_check(self.map)
+			scan_size = len(scan_map)
 			#origin + pose = index.
 			start_point = [(self.origin[0] + self.pose[0]-scan_size/2),
 			(self.origin[1] + self.pose[1] + scan_size/2)]
@@ -74,7 +73,7 @@ class Map():
 					self.map_[start_point[0] + i][start_point[1] + j] = max(new_scan, reference)
 
 
-		def bound_check(map_): #checks if new scan is in bounds, expands map if not.
+		def bound_check(self, map_): #checks if new scan is in bounds, expands map if not.
 			#size of the square-map scan is len(map), integer coordiinates.
 			#Check the sign of the difference in distance to determine where to expand the map if needed.
 
@@ -112,85 +111,13 @@ class Map():
 			self.origin = (self.origin[0] + translate_origin_x, self.origin[1] + translate_origin_y)
 
 	def show_map():
-=======
-	def __init__(self, map_, resolution): #Size is a tuple of [x,y] in meters.
-		self.map_ = map_
-		self.resolution = resolution
-		self.size = (len(self.map_), len(self.map_[0]))
-
-		self.origin = (math.floor(self.size[0]/2), math.floor(self.size[1]/2))
-		self.pose = [] #[x, y, theta], is always relative.  It's the measure of distance from origin.
-
-	def stitch(scan_map):
-		#Take current map, reference to origin.  Compare to scan, reference to pose.
-		#Transform (only location) scan to have the same orientation as origin, and merge.
-		bound_check(scan_map)
-		scan_size = len(scan)
-		#origin + pose = index.
-		start_point = [(self.origin[0] + self.pose[0]-scan_size/2),
-		(self.origin[1] + self.pose[1] + scan_size/2)]
-
-		for i in scan_size: #compare the scan_map to the scan.
-			for j in scan_size:
-				new_scan  = scan_map[i][j]
-				reference = self.map[start_point[0] + i][start_point[1] + j] #equivalent point on map
-				#based on priority. 1 is a solid wall and always takes priority.
-				#0 is navigable space, and -1 is unknown.
-				self.map_[start_point[0] + i][start_point[1] + j] = max(new_scan, reference)
-
-
-	def bound_check(map_): #checks if new scan is in bounds, expands map if not.
-		#size of the square-map scan is len(map), integer coordiinates.
-		#Check the sign of the difference in distance to determine where to expand the map if needed.
-
-		dist_x, dist_y = round(self.pose[0])/resolution,
-		round(self.pose[1])/resolution #convert distance to integer coordinates.
-		mapsize_x = self.size[0]
-		mapsize_y = self.size[1]
-		#Map expansion works by creating a new map with the expanded size.
-		#First it determines if the map needs to be expanded by looking at
-		# if the bot is within the current map.  Then it calculates the
-		#amount that needs to be expanded, then determines
-		#if the origin needs to be translated (if negative), since the map can only
-		#be expanded by adding indices.
-		map_expand_x = 0
-		map_expand_y = 0
-		translate_origin_x = 0
-		translate_origin_y = 0
-
-		if (dist_x >= 0):
-			if (dist_x + (len(map_)/2) > mapsize_x/2):
-				map_expand_x = (dist_x)
-		if (dist_y >= 0):
-			if (dist_y + (len(map_)/2) > mapsize_y/2):
-				map_expand_y = dist_y
-		if (dist_x <= 0):
-			if (abs(dist_x - (len(map_)/2)) > mapsize_x/2):
-				map_expand_x = abs(dist_x)
-				translate_origin_x = map_expand_x
-		if (dist_y <= 0):
-			if (abs(dist_y - (len(map_)/2)) > mapsize_y/2):
-				map_expand_y = abs(dist_y)
-				translate_origin_y = map_expand_y
-
-		#now to translate the origin
-		self.origin = (self.origin[0] + translate_origin_x, self.origin[1] + translate_origin_y)
-
-	def show_map(self):
->>>>>>> 7ede1260b2ac1f1b818899df6f637d92e7767812
 		# Create a 1024x1024x3 array of 8 bit unsigned integers
 		display = np.zeros((len(self.map_), len(self.map_[0]), 3), dtype=np.uint8)
 
-<<<<<<< HEAD
-		for x in range():
-			for y in range():
-
-=======
 		for x in range(len(self.map_)):
 			for y in range(len(self.map_[0])):
 				reading = self.map_[x][y]
 				display[x][y] = ([0, 0, 0] if (reading == WALL) else ([255, 255, 255] if (reading == EMPTY) else [100, 100, 100]))
->>>>>>> 7ede1260b2ac1f1b818899df6f637d92e7767812
 
 		img = smp.toimage(display)
 		img.show()
